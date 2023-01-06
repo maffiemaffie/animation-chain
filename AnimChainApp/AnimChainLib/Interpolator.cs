59ce1d0f823f943ba2d96d916eadd462e807c6be
maffiemaffie <111717714+maffiemaffie@users.noreply.github.com>
2023-01-05 22:56:16 -0500

    hi

---
+++ AnimChainApp/AnimChainLib/Interpolator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace AnimChainLib
+{
+	/// <summary>
+	/// Abstract class <c>Interpolator</c> rescales time to a factor accepted
+	/// by the transformer.
+	/// </summary>
+	public abstract class Interpolator
+	{
+		private int start;
+		private int end;
+
+		public int Duration { get { return end - start; } }
+
+		public abstract double Interpolate(int frame);
+
+		public Interpolator(int start, int end)
+		{
+			this.start = start;
+			this.end = end;
+		}
+
+		public Interpolator(int start, int duration): this()
+	}
+}
+
