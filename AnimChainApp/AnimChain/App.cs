6cbb2ff243a802dbdeeca51b316e32cb97e938f4
Mafffie Cohen <mrc6113@rit.edu>
2023-01-16 16:15:32 -0500

    more work on anim chain app

--- AnimChainApp/AnimChain/App.cs
+++ AnimChainApp/AnimChain/App.cs
@@ -2,9 +2,21 @@
 {
     public class AnimChainApp
     {
+        private Configs configs;
+        private AnimChain chain;
+
         private class Configs
         {
-            public readonly (int Width, int Height) Resolution
+            public readonly (int Width, int Height) Resolution;
+            public readonly int FrameDelay;
+            public readonly double FrameRate;
+            public readonly FileFormat Format;
+        }
+
+        private enum FileFormat
+        {
+            GIF,
+            MP4
         }
     }
 
