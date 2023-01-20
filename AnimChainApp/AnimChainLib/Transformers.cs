cf11b5c4c1a5e91760a5109258d17b103578550b
Mafffie Cohen <mrc6113@rit.edu>
2023-01-20 11:57:41 -0500

    more work on indiv transformers

---
+++ AnimChainApp/AnimChainLib/Transformers.cs
@@ -0,0 +1,17 @@
+﻿namespace AnimChainLib.Transformers
+{
+    public class FlyIn : ITransformer<Point>
+    {
+        private readonly double direction;
+
+        public Point Transform(Point value, double factor)
+        {
+            double _x = value.X;
+            double _y = value.Y;
+
+            _x += 2 * factor * Math.Cos(direction);
+
+            return new Point(_x, _y);
+        }
+    }
+}
