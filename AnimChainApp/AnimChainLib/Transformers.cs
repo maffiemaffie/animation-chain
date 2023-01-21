456840cea0361b9e8825b2c50a93c89b217a1a12
Mafffie Cohen <mrc6113@rit.edu>
2023-01-20 22:28:23 -0500

    more work on indiv transformers

--- AnimChainApp/AnimChainLib/Transformers.cs
+++ AnimChainApp/AnimChainLib/Transformers.cs
@@ -14,4 +14,67 @@
             return new Point(_x, _y);
         }
     }
+
+    public class SplitIn : ITransformer<Point>
+    {
+        private readonly Direction direction;
+
+        public Point Transform(Point value, double factor)
+        {
+            double _x = value.X;
+            double _y = value.Y;
+
+            if (direction == Direction.Horizontal)
+            {
+                if (value.X < 0) _x = -factor;
+                else _x = factor;
+            }
+            else
+            {
+                if (value.Y < 0) _y = -factor;
+                else _y = factor;
+            }
+
+            return new Point(_x, _y);
+        }
+
+        private enum Direction
+        {
+            Horizontal,
+            Vertical
+        }
+    }
+
+    public class Spin : ITransformer<Point>
+    {
+        private readonly double rotation;
+
+        public Point Transform(Point value, double factor)
+        {
+            double _x = value.X;
+            double _y = value.Y;
+
+            double _rotation = rotation * factor;
+
+            _x = _x * Math.Cos(_rotation) - _y * Math.Sin(_rotation);
+            _y = _x * Math.Sin(_rotation) + _y * Math.Cos(_rotation);
+
+            return new Point(_x, _y);
+        }
+    }
+
+    public class Swivel : ITransformer<Point>
+    {
+        private readonly double rotation;
+        private readonly double direction;
+        public Point Transform(Point value, double factor)
+        {
+            double _x = value.X;
+            double _y = value.Y;
+
+            double _rotation = rotation * factor;
+
+            _x = _x * (Math.Cos(_rotation) + _x * (1 - Math.Cos(_rotation))) + _y * _x * _y * (1 - Math.Cos(_rotation));
+        }
+    }
 }
